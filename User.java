9616621a963b65a741ad7b054a6eb79a2169d093
ZoePaganini <77688974+ZoePaganini@users.noreply.github.com>
2021-10-06 18:17:58 +0200

    Add files via upload

---
+++ User.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spdvi;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Alumne
+ */
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String gender;
+    private boolean isAlive;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public boolean isIsAlive() {
+        return isAlive;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder strUser = new StringBuilder();
+        strUser.append(this.id + ": " + this.lastName + ", " + this.firstName + " - " + age + " years old - " + this.gender + " - ");
+        return "User{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", birthDate=" + birthDate + ", gender=" + gender + ", isAlive=" + isAlive + '}';
+    }
+
+    public User(int id, String firstName, String lastName, LocalDate birthDate, String gender, boolean isAlive) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.isAlive = isAlive;
+    }
+}
+
+
